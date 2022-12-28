87fcaab39d892bb2781b30f8fe8eea5a1165432f
Lucas de Oliveira Neves <LONeves@rd.com.br>
2022-12-28 11:36:07 -0300

    Adding a 404 page

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(adminRoutes);
 app.use(shopRoutes);
 
 
+app.use((req, res, next) => {
+  res.status(404).send('<h2>Page not found</h2>');
+})
+
 // const server = http.createServer(app);
 // server.listen(3000);
 
