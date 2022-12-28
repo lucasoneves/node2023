5a8c68136077cb70a0bd229cfe39214f9bb4c931
Lucas de Oliveira Neves <LONeves@rd.com.br>
2022-12-28 11:43:15 -0300

    Filtering paths

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false})) // Body parsing 
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
