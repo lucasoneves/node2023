d0d6fc428caa58fbfe63653bfd6ab06e27ff1969
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-21 09:48:52 -0300

    Adding controllers

--- routes/shop.js
+++ routes/shop.js
@@ -6,13 +6,9 @@ const router = express.Router();
 
 const adminData = require("./admin");
 
-// GET method uses exact path
-router.get("/", (req, res, next) => {
-  
-  const products = adminData.products;
+const productsController = require('../controllers/products');
 
-  // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/shop", hasProducts: products.length > 0 });
-});
+// GET method uses exact path
+router.get("/", productsController.getProducts);
 
 module.exports = router;
