0644c28115c0a0aac330cad059b724f0166ea9d4
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-23 00:44:19 -0300

    Adding model

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
@@ -6,10 +9,37 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
+    const p = path.join(
+        path.dirname(process.mainModule.filename),
+        "data",
+        "products.json"
+      );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+
+      return JSON.parse(fileContent);
+    });
     return products;
   }
 };
